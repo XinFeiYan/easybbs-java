8fa67d8acc98ed56c7858251d547c60d25df5c26
XinKong <3310156398@qq.com>
2023-10-14 16:02:04 +0800

    项目搭建完成

---
+++ easybbs-admin/src/main/java/com/jiang/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.jiang.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/test")
+    public String test(){
+        return "admin";
+    }
+}
